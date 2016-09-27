02a288f633dff4940f99cda78da0c87f9686f5fd
Aby <Aby>
2016-09-27 23:46:23 +0530

    adding files

---
+++ Project/script.js
@@ -0,0 +1,5 @@
+function openWin() {
+
+ var a=document.getElementById("frm1").elements[0].value;
+ window.open("http://www.intel.in/buy/in/en/catalog/components/boxedprocessors/6th-generation-intel-core-"+a);
+}
